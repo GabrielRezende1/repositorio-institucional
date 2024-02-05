bcaf2a999e01f4413adb57499bb5c59192552d31
Gabriel Santos Rezende <gabrielthel9@gmail.com>
2024-02-05 12:35:38 -0300

    feat: added data to route response and redirection response to search feature

--- root/backend/express/routes/home.js
+++ root/backend/express/routes/home.js
@@ -22,31 +22,39 @@ router.get("/", async (req, res) => {
 		}
 	});
 
-	//Query code
 	let query = req.query;
-	if(!(query.search == undefined)) { //only search db if query exists
-		console.log("Query existe!");
-		console.log(query.search);
+	//Query code
+	if(query.search == undefined) { //only search db if query exists
+		const documentTypes = doctypes.map((el, i, arr) => {
+			return el = el.tipo;
+		});
+	
+		res.json({ documentTypes });
+		return;
 	}
+	
+	console.log("Query existe!");
+	console.log(query);
+	res.redirect(`/documento/?search=${query.search}&page=${query.page}`);
 
-	res.json({
-		msg: "Rota '/' alcançada!", 
-		data: doctypes,
-		query
-	});
 });
 //GET /apresentacao
-router.get("/apresentacao", async(req, res) => { //TODO simples apresentação em html
+router.get("/apresentacao", async(req, res) => {
 	res.json({
-		msg: "Rota '/apresentacao' alcançada!",
-		apresentacao: "Inserir apresentação"
+		apresentacao: "Bem-vindo ao repositório institucional da FAETERJ-Paracambi.",
+		missao: "Armazenar, preservar, divulgar e oferecer acesso à produção científica e institucional da FAETER-Paracambi.",
+		objetivo: "Contribuir para o aumento da visibilidade da produção científica da FAETERJ-Paracambi; Preservar a memória intelectual da faculdade; Reunir em um único local virtual e de forma permanente a produção científica e institucional; Disponibilizar o livre acesso aos conteúdos digitais; Ampliar e facilitar o acesso à produção científica de uma forma geral."
 	});
 });
 //GET /faq
-router.get("/faq", async(req, res) => { //TODO simples apresentação em html
+router.get("/faq", async(req, res) => {
 	res.json({
-		msg: "Rota '/apresentacao' alcançada!",
-		faq: "Inserir FAQ"
+		q1: "O que é um Repositório Institucional?",
+		a1: "Um Repositório Institucional é um ambiente digital dedicado ao armazenamento, acesso, divulgação e preservação da produção intelectual de uma instituição.",
+		q2: "Quais são os objetivos do Repositório Institucional da FAETER-Paracambi(RI-FAETERJ-PRC)?",
+		a2: "Unificar em plataforma comum a produção intelectual da Faculdade de forma a maximizar a visibilidade e o acesso; preservar e conservar a produção intelectual da Faculdade; potencializar o intercâmbio da FAETERJ-Paracambi com outras instituições com vistas a promover a democratização do conhecimento.",
+		q3: "Quais documentos são disponibilizados no RI-FAETERJ-PRC?",
+		a3: "Inicialmente, serão disponibilizados no Repositório: artigos de periódicos e de eventos; livros, capítulos de livros; dissertações; monografias; trabalhos de conclusão de curso, teses; tutoriais e políticas do site."
 	});
 });
 
