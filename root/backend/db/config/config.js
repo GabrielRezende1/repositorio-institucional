f312f015aca4f20f2b74ec3341b6df0a4bc1c4b1
Gabriel Santos Rezende <gabrielthel9@gmail.com>
2026-03-10 19:22:12 -0300

    fix: add dialectOptions for client encoding in database config

--- root/backend/db/config/config.js
+++ root/backend/db/config/config.js
@@ -6,20 +6,29 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "dialectOptions": {
+      "client_encoding": process.env.DB_CLIENT_ENCODING,
+    }
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "dialectOptions": {
+      "client_encoding": process.env.DB_CLIENT_ENCODING,
+    }
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "dialectOptions": {
+      "client_encoding": process.env.DB_CLIENT_ENCODING,
+    }
   }
 }
