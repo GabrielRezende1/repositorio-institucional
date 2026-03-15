03bf34f1c1c5a23b1a4521f2f9a68efc8aa76172
Gabriel Santos Rezende <gabrielthel9@gmail.com>
2026-03-15 12:00:16 -0300

    fix: restructure sequelize database configuration and add options

--- root/backend/db/config/config.js
+++ root/backend/db/config/config.js
@@ -5,30 +5,39 @@ module.exports = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    "dialectOptions": {
-      "client_encoding": process.env.DB_CLIENT_ENCODING,
+    "options": {
+      "host": process.env.DB_HOST,
+      "port":process.env.DB_PORT,
+      "dialect": process.env.DB_DIALECT,
+      "dialectOptions": {
+        "client_encoding": process.env.DB_CLIENT_ENCODING,
+      }
     }
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    "dialectOptions": {
-      "client_encoding": process.env.DB_CLIENT_ENCODING,
+    "options": {
+      "host": process.env.DB_HOST,
+      "port":process.env.DB_PORT,
+      "dialect": process.env.DB_DIALECT,
+      "dialectOptions": {
+        "client_encoding": process.env.DB_CLIENT_ENCODING,
+      }
     }
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    "dialectOptions": {
-      "client_encoding": process.env.DB_CLIENT_ENCODING,
+    "options": {
+      "host": process.env.DB_HOST,
+      "port":process.env.DB_PORT,
+      "dialect": process.env.DB_DIALECT,
+      "dialectOptions": {
+        "client_encoding": process.env.DB_CLIENT_ENCODING,
+      }
     }
   }
 }
